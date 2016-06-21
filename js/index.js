6d57dfabaef01964cbea6b4422ace07ab8ba39c7
n8mr2 <nilpaddo@gmail.com>
2016-06-21 20:14:34 +0600

    Get weather data from OpenWeatherApi according to the users current location

--- js/index.js
+++ js/index.js
@@ -14,20 +14,19 @@ $(document).ready(function() {
         navigator.geolocation.getCurrentPosition(doGeo);
     }
 
+    // Getting weather data
     function saving_coordinates(coordinates) {
         var lat = parseFloat(coordinates.split(',')[0]),
             lng = parseFloat(coordinates.split(',')[1]);
-        var latlng = new google.maps.LatLng(lat, lng);
-        var geocoder = geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-            'latLng': latlng
-        }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[1]) {
-                    var address = results[1].formatted_address.split(',');
-                    // alert(address[1]);
-                }
-            }
+        var open_weather_url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=1f144e5252b7bdd3bc119a2e123ed8f1";
+        $.ajax({
+        	method: 'GET',
+        	url: open_weather_url,
+        	dataType: 'json',
+        	success: function(data){
+        		console.log(data);
+        	}
         });
     }
 });
+
