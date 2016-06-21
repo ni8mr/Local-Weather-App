a1ba92acd75ba893483a5f750378d3373f8c16db
n8mr2 <nilpaddo@gmail.com>
2016-06-21 22:01:30 +0600

    Get city name of the user - v1 (Google API key is not working)

--- js/index.js
+++ js/index.js
@@ -13,20 +13,32 @@ $(document).ready(function() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(doGeo);
     }
-
     // Getting weather data
     function saving_coordinates(coordinates) {
         var lat = parseFloat(coordinates.split(',')[0]),
             lng = parseFloat(coordinates.split(',')[1]);
         var open_weather_url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=1f144e5252b7bdd3bc119a2e123ed8f1";
         $.ajax({
-        	method: 'GET',
-        	url: open_weather_url,
-        	dataType: 'json',
-        	success: function(data){
-        		console.log(data);
-        	}
+            method: 'GET',
+            url: open_weather_url,
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+            }
+        });
+        //getting city name
+        var latlng = new google.maps.LatLng(lat, lng)
+        var geocoder = geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            'latLng': latlng
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    var address = results[1].formatted_address.split(',')
+                    var city = address[1];
+                    console.log(city);		
+                }
+            }
         });
     }
 });
-
