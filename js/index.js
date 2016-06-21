8f0ff861f942beb7e75991ba7cc4da3b61bd9676
ni8mr2 <nilpaddo@gmail.com>
2016-06-21 15:27:16 +0600

    Get users current city.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
-  // calculating body width for styling
-  var body_width = $("body").width();
-  var header_width = $('header').width();
-  // Styling header
-  $('header').css('margin-left',(body_width - header_width)/2);
+    // calculating body width for styling
+    var body_width = $("body").width();
+    var header_width = $('header').width();
+    // Styling header
+    $('header').css('margin-left', (body_width - header_width) / 2);
+    //Getting the geo-location of the user
+    function doGeo(position) {
+        var latitude = position.coords.latitude,
+            longitude = position.coords.longitude;
+        saving_coordinates(latitude + "," + longitude);
+    }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(doGeo);
+    }
 
+    function saving_coordinates(coordinates) {
+        var lat = parseFloat(coordinates.split(',')[0]),
+            lng = parseFloat(coordinates.split(',')[1]);
+        var latlng = new google.maps.LatLng(lat, lng);
+        var geocoder = geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            'latLng': latlng
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    var address = results[1].formatted_address.split(',');
+                    // alert(address[1]);
+                }
+            }
+        });
+    }
 });
